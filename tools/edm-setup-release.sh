f4e91c04d043447dc830cb90815d32a40abcd613
Richard Hu <richard.hu@technexion.com>
2017-05-16 11:50:40 +0800

    tools/edm-setup-release.sh: add 'lvds15' display option for 15 inch 1368x768 LVDS panel

--- tools/edm-setup-release.sh
+++ tools/edm-setup-release.sh
@@ -249,9 +249,10 @@ if [ -f $UENV_PATH/uEnv.txt ] ; then
 fi
 
 if [ "$CPU_TYPE" == 'imx6' ]; then
-	if [ "$DISPLAY" != "lvds7" ] && [ "$DISPLAY" != "lvds10" ] && [ "$DISPLAY" != "hdmi720p" ] && [ "$DISPLAY" != "hdmi1080p" ]  \
+	if [ "$DISPLAY" != "lvds7" ] && [ "$DISPLAY" != "lvds10" ] && [ "$DISPLAY" != "lvds15" ] \
+	&& [ "$DISPLAY" != "hdmi720p" ] && [ "$DISPLAY" != "hdmi1080p" ]  \
 	&& [ "$DISPLAY" != "lcd" ] && [ "$DISPLAY" != "lvds7_hdmi720p" ] && [ "$DISPLAY" != "custom" ]; then
-		echo "Display is wrong. Please assign DISPLAY as one of lvds7, lvds10, hdmi720p, hdmi1080p, lcd, lvds7_hdmi720p, lcd, custom"
+		echo "Display is wrong. Please assign DISPLAY as one of lvds7, lvds10, lvds15, hdmi720p, hdmi1080p, lcd, lvds7_hdmi720p, lcd, custom"
 		if [ "$BASEBOARD" == "tc0700" ]; then
 			echo "setting lvds7 as default display"
 			DISPLAY="lvds7"
